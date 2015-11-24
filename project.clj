e16edb0df3e508c497906bd58e5e18d82bca7ab3
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2015-11-24 21:37:13 +0200

    Version 0.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hh-sync "0.0.6-SNAPSHOT"
+(defproject hh-sync "0.0.6"
   :description "a command-line utility for syncing workouts from Endomondo to HeiaHeia"
   :url "https://github.com/propan/hh-sync"
   :license {:name "Eclipse Public License"
